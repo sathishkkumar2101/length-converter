342c72aa80d3a1239ba844ac24b950f6d04adaa4
sathishkkumar2101 <sathishkkumar2101@gmail.com>
2024-03-06 07:46:33 +0530

    first cimmit
    first date:06-03-2024

---
+++ demo.js
@@ -0,0 +1,36 @@
+// var a=10
+// console.log(a)
+// function abc()
+// {
+// console.log("function is working")
+// }
+// abc()
+
+// var a=10
+// var b=20
+// function add()
+// {
+// console.log(a+b)
+// }
+// add()
+
+// var factor = "vijay";
+// var fplayer = "dhoni";
+// var fmovie = "leo";
+// function favourite() {
+//   console.log(
+//     "favourite actor:",
+//     factor,
+//     "\nfavourite palyer:",
+//     fplayer,
+//     "\nfavourite movie:",
+//     fmovie
+//   );
+// }
+// favourite();
+
+function area(length, breadth) {
+    var area=length*breadth
+    console.log("Area:"+area)
+}
+area(12,34)
